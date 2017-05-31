b34c6d0945602aeb2729fb3aa63da3223e8e8967
yichengjie <626659321@qq.com>
2017-05-31 11:05:05 +0800

    s

--- layout1/src/index.jsx
+++ layout1/src/index.jsx
@@ -4,13 +4,16 @@ import React,{Component} from 'react';
 import ReactDOM from 'react-dom';
 //引入组件
 import HelloComp from './components/HelloComp.jsx' ;
+import Layout1 from './components/Layout1.jsx' ;
+import Layout2 from './components/Layout2.jsx' ;
+import Layout3 from './components/Layout3.jsx' ;
+import './styles/index.scss' ;
 
 let appNode = document.createElement('div') ;
 appNode.id = 'app' ;
-appNode.className= 'container' ;
 document.body.appendChild(appNode) ;
 
 ReactDOM.render(
-  <HelloComp />,
+  <Layout3 />,
   appNode
 );
