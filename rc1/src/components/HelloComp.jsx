f0fb1d9b446708492b73578a37822634736771e3
yichengjie <626659321@qq.com>
2017-05-31 18:01:21 +0800

    s

--- rc1/src/components/HelloComp.jsx
+++ rc1/src/components/HelloComp.jsx
@@ -1,9 +1,16 @@
-import  React,{Component} from 'react'
+import  React,{Component} from 'react' ;
+var notification = Notification.newInstance();
 class HelloComp extends Component {
+    handleClick(){
+        notification.notice({
+            content: 'content'
+        });
+    }
     render(){
         return (
             <div>
                 <h1>hello world fdfsd sdd</h1>
+                <button onClick={this.handleClick}>best wishes</button>
             </div>
         )
     }
--- rc1/src/index.jsx
+++ rc1/src/index.jsx
@@ -4,6 +4,7 @@ import React,{Component} from 'react';
 import ReactDOM from 'react-dom';
 //引入组件
 import HelloComp from './components/HelloComp.jsx' ;
+import DomAlign1 from './components/DomAlign1.jsx' ;
 
 let appNode = document.createElement('div') ;
 appNode.id = 'app' ;
