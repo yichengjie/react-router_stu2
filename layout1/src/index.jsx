6e88480471afd7360775e49b322d370b6d1b9b2c
yichengjie <626659321@qq.com>
2017-05-31 10:37:50 +0800

    测试ant design的layout

--- layout1/src/index.jsx
+++ layout1/src/index.jsx
@@ -5,7 +5,12 @@ import ReactDOM from 'react-dom';
 //引入组件
 import HelloComp from './components/HelloComp.jsx' ;
 
+let appNode = document.createElement('div') ;
+appNode.id = 'app' ;
+appNode.className= 'container' ;
+document.body.appendChild(appNode) ;
+
 ReactDOM.render(
   <HelloComp />,
-  document.getElementById('app')
+  appNode
 );
---
+++ project-template/src/index.jsx
@@ -0,0 +1,16 @@
+
+'use strict';
+import React,{Component} from 'react';
+import ReactDOM from 'react-dom';
+//引入组件
+import HelloComp from './components/HelloComp.jsx' ;
+
+let appNode = document.createElement('div') ;
+appNode.id = 'app' ;
+appNode.className= 'container' ;
+document.body.appendChild(appNode) ;
+
+ReactDOM.render(
+  <HelloComp />,
+  appNode
+);
