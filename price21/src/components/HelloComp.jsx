7af4576c4f77047e1d58c442b120552a2a0dda31
yichengjie <626659321@qq.com>
2017-06-02 16:53:05 +0800

    s

--- price21/src/components/HelloComp.jsx
+++ price21/src/components/HelloComp.jsx
@@ -1,11 +1,25 @@
 import  React,{Component} from 'react' ;
-import { Input } from 'antd';
+import { Input,Radio  } from 'antd';
+const RadioGroup = Radio.Group;
 class HelloComp extends Component {
+    constructor(props){
+        super(props) ;
+    }
+    state = {
+        name:'yicj',
+        radio:3
+    } ;
     render(){
         return (
             <div>
-                <Input placeholder="Basic usage" />
+                <Input value ={this.state.value}/>
                 <h1>hello world 我会自动刷新哦</h1>
+                <RadioGroup  value={this.state.radio}>
+                    <Radio value={1}>A</Radio>
+                    <Radio value={2}>B</Radio>
+                    <Radio value={3}>C</Radio>
+                    <Radio value={4}>D</Radio>
+                </RadioGroup>
             </div>
         )
     }
